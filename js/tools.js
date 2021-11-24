b8ebeebe4521d7b8342a6aeb57ec7f831ceffd3b
eachLee <eachli@163.com>
2021-11-24 16:40:34 +0800

    修改写法为es6的写法

--- js/tools.js
+++ js/tools.js
@@ -1,47 +1,45 @@
-var ToolsObj = function () { }
-ToolsObj.prototype = {
+let ToolsObj = class ToolsObj {
+  //获取指定位数的四舍五入 num为数字 d为尾数
+  roundNum(num, d) { return Number(Math.round(n + "e" + d) + "e-" + d) }
+
   //去除文本里的html标签 返回替换后的文本
-  stripHtml: function (html) {
-    return (new DOMParser().parseFromString(html, 'text/html')).body.textContent || ''
-  },
+  stripHtml(html) { return (new DOMParser().parseFromString(html, 'text/html')).body.textContent || '' }
 
   // 获取指定范围的随机数 返回整数 包含min和max值
-  getRandomNum: function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  },
+  getRandomNum(min, max) { Math.floor(Math.random() * (max - min + 1) + min) }
   // 获取当前地址的参数key为参数名
-  getUrlParam: function (key) {
-    var reg = new RegExp('[?|&]' + key + '=([^&]+)')
-    var match = location.search.match(reg)
+  getUrlParam(key) {
+    let reg = new RegExp('[?|&]' + key + '=([^&]+)')
+    let match = location.search.match(reg)
     return match && match[1]
-  },
+  }
   //结构多维数组为扁平化数组 返回结构后得数组
-  reArr: function (arr) {
+  reArr(arr) {
     let newArr = [].concat(...arr);
     return newArr.some(Array.isArray) ? reArr(newArr) : newArr;
-  },
+  }
   // 获取指定长度的随机字符串，参数为num 为指定的长度 返回字符串
-  getrandomstr: function (num) {
-    var str = ''
-    for (var i = str.length; i < num; i = str.length) {
+  getrandomstr(num) {
+    let str = ''
+    for (let i = str.length; i < num; i = str.length) {
       str += Math.random()
         .toString(36)
         .slice(2)
     }
     str = str.slice(0, num)
     return str
-  },
+  }
   // 获取url里的参数值 并返回一个包含对应参数相应的object对象 url 为指定网址
-  getUrlParam: function (url = location.href) {
-    var urlArr = [],
+  getUrlParam(url = location.href) {
+    let urlArr = [],
       urlObj = {}
     if (url.indexOf('?') > -1) {
       urlArr = url.split('?')
       if (urlArr[1].indexOf('&') > -1) {
         urlArr = urlArr[1].split('&')
-        for (var i = 0; i < urlArr.length; i++) {
-          var ele = urlArr[i]
-          var eleArr = []
+        for (let i = 0; i < urlArr.length; i++) {
+          let ele = urlArr[i]
+          let eleArr = []
           if (ele.indexOf('=') > -1) {
             eleArr = ele.split('=')
             urlObj[eleArr[0]] = eleArr[1]
@@ -49,65 +47,65 @@ ToolsObj.prototype = {
         }
       } else {
         if (urlArr[1].indexOf('=') > -1) {
-          var ele2Arr = urlArr[1].split('=')
+          let ele2Arr = urlArr[1].split('=')
           urlObj[ele2Arr[0]] = ele2Arr[1]
         }
       }
     }
     return urlObj
-  },
+  }
   // 提升层级的一个函数 添加广告之后把高于添加广告层级的降低为9999
-  upzindex: function () {
-    var allA = document.getElementsByTagName('a'),
+  upzindex() {
+    let allA = document.getElementsByTagName('a'),
       allIfr = document.getElementsByTagName('iframe'),
       allDiv = document.getElementsByTagName('div')
-    for (var i = 0; i < allA.length; i++) {
-      var eleA = allA[i]
+    for (let i = 0; i < allA.length; i++) {
+      let eleA = allA[i]
       if (eleA.style.zIndex && eleA.style.zIndex > 9999) {
         eleA.style.zIndex = 9999
-        var aBottom = window.getComputedStyle(eleA).bottom.slice(0, -2)
+        let aBottom = window.getComputedStyle(eleA).bottom.slice(0, -2)
         if (aBottom < 50) {
-          var botA = window.getComputedStyle(eleA).height
+          let botA = window.getComputedStyle(eleA).height
           eleA.style.bottom = '-' + botA
         }
       }
     }
-    for (var n = 0; n < allIfr.length; n++) {
-      var eleIfr = allIfr[n]
+    for (let n = 0; n < allIfr.length; n++) {
+      let eleIfr = allIfr[n]
       if (eleIfr.style.zIndex && eleIfr.style.zIndex > 9999) {
         eleIfr.style.zIndex = 9999
-        var ifrBottom = window.getComputedStyle(eleIfr).bottom.slice(0, -2)
+        let ifrBottom = window.getComputedStyle(eleIfr).bottom.slice(0, -2)
         if (ifrBottom < 50) {
-          var botIfr = window.getComputedStyle(eleIfr).height
+          let botIfr = window.getComputedStyle(eleIfr).height
           eleIfr.style.bottom = '-' + botIfr
         }
       }
     }
-    for (var m = 0; m < allDiv.length; m++) {
-      var eleDiv = allDiv[m]
+    for (let m = 0; m < allDiv.length; m++) {
+      let eleDiv = allDiv[m]
       if (eleDiv.id && eleDiv.id.indexOf('_so_') > -1) {
         continue
       }
       if (eleDiv.style.zIndex && eleDiv.style.zIndex > 9999) {
         eleDiv.style.zIndex = 9999
-        var divBottom = window.getComputedStyle(eleDiv).bottom.slice(0, -2)
+        let divBottom = window.getComputedStyle(eleDiv).bottom.slice(0, -2)
         if (divBottom < 50) {
-          var bot = window.getComputedStyle(eleDiv).height
+          let bot = window.getComputedStyle(eleDiv).height
           eleDiv.style.bottom = '-' + bot
         }
       }
     }
-  },
-  getCookies: function (cookies, name) {
+  }
+  getCookies(cookies, name) {
     // cookies 为string 必填项 cookie字符串
     // name 要获取的cookie名
     // 返回值为string
-    var cookiesArr = []
+    let cookiesArr = []
     if (cookies && name) {
       cookiesArr = cookies.split('; ')
-      for (var i = 0; i < cookiesArr.length; i++) {
-        var ele = cookiesArr[i]
-        var index = ele.indexOf(name + '=')
+      for (let i = 0; i < cookiesArr.length; i++) {
+        let ele = cookiesArr[i]
+        let index = ele.indexOf(name + '=')
         if (index > -1) {
           return ele.split('=')[1]
         }
@@ -115,42 +113,40 @@ ToolsObj.prototype = {
     } else {
       return ''
     }
-  },
-  getCookie: function (name) {
+  }
+  getCookie(name) {
     //name 值为cookie名
     let arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
     if (arr != null) return unescape(arr[2]);
     return null;
-  },
+  }
   // 复制指定文本到剪切板上 参数为要复制的文本
-  copyText: function (text) {
-    var copyBox = document.createElement('input')
+  copyText(text) {
+    let copyBox = document.createElement('input')
     copyBox.value = text
     document.body.appendChild(copyBox)
     copyBox.select()
     document.execCommand('copy')
     copyBox.style.display = 'none'
     copyBox.remove()
-  },
+  }
   //模拟实现的一个sleep函数 es6以上支持 单位为ms
-  sleep: function (ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  },
+  sleep(ms) { new Promise(resolve => setTimeout(resolve, ms)) }
   // 判断设备是否支持触屏 返回boolean值
-  hasTouch: function () {
-    var touchObj = {}
+  hasTouch() {
+    let touchObj = {}
     touchObj.isSupportTouch = 'ontouchend' in document ? true : false
     touchObj.isEvent = touchObj.isSupportTouch ? 'touchstart' : 'click'
     return touchObj.isSupportTouch
-  },
+  }
   // 深复制对象 返回复制的对象
-  deelClone: function (obj) {
+  deelClone(obj) {
     if (!obj) return
     // 一个检测传入的对象是否是指定类型的函数 返回boolean
     function isType(obj, type) {
       if (typeof obj !== 'object') return false
-      var objType = Object.prototype.toString.call(obj).toLowerCase()
-      var flag = false
+      let objType = Object.prototype.toString.call(obj).toLowerCase()
+      let flag = false
       switch (type) {
         case 'array':
           flag = objType === '[object array]'
@@ -168,17 +164,17 @@ ToolsObj.prototype = {
     }
     // 获取regexp的参数 返回参数值
     function getRegexp(reg) {
-      var flags = ''
+      let flags = ''
       if (reg.global) flags += 'g'
       if (reg.ignoreCase) flags += 'i'
       if (reg.multiline) flags += 'm'
       return flags
     }
     //维护两个储存循环引用的数组
-    var parents = []
-    var children = []
+    let parents = []
+    let children = []
     function cloneObj(obj) {
-      var child, proto
+      let child, proto
       if (typeof obj === 'function') return obj
       if (typeof obj !== 'object') return obj
       if (isType(obj, 'array')) {
@@ -203,7 +199,7 @@ ToolsObj.prototype = {
       }
 
       // 处理循环引用
-      var index = parents.indexOf(children)
+      let index = parents.indexOf(children)
       if (index > -1) {
         // 如果父数组存在本对象,说明之前已经被引用过,直接返回此对象
         return children[index]
@@ -211,7 +207,7 @@ ToolsObj.prototype = {
       parents.push(obj)
       children.push(child)
       //  对对象做特殊处理
-      for (var key in obj) {
+      for (let key in obj) {
         if (obj.hasOwnProperty(key)) {
           // 递归调用
           child[key] = cloneObj(obj[key])
@@ -220,7 +216,7 @@ ToolsObj.prototype = {
       return child
     }
     return cloneObj(obj)
-  },
+  }
   /**
    * 克隆对象
    * @param {Object} obj - 被判断的对象
@@ -228,7 +224,7 @@ ToolsObj.prototype = {
    * @return {Object} - 返回新对象
    */
   //Fan神的方法 稍微修改版 有个别需要单独处理 如Date对象...
-  deelClone2: function (obj, cloneAll) {
+  deelClone2(obj, cloneAll) {
     switch (true) {
       case obj === null || obj === undefined:
       case typeof obj === 'number':
@@ -241,15 +237,15 @@ ToolsObj.prototype = {
           }
           : obj
       case obj instanceof RegExp:
-        var reg = new RegExp(obj.source, obj.flags)
+        let reg = new RegExp(obj.source, obj.flags)
         reg.lastIndex = obj.lastIndex
         return reg
       case obj instanceof Date:
-        var date = new Date(obj.getTime())
+        let date = new Date(obj.getTime())
         return date
       case typeof obj === 'object' && !!obj:
-        var newObj = obj instanceof Array ? [] : {}
-        for (var i in obj) {
+        let newObj = obj instanceof Array ? [] : {}
+        for (let i in obj) {
           if (i && obj.hasOwnProperty(i)) {
             newObj[i] = cloneAll ? cloneObject(obj[i]) : obj[i]
           }
@@ -258,7 +254,7 @@ ToolsObj.prototype = {
     }
 
     return obj
-  },
+  }
   //根据身份证获取出生年月、性别、年龄
   getIdCardAnalysis(card, num) {
     if (num == 1) {
@@ -279,36 +275,36 @@ ToolsObj.prototype = {
     }
     if (num == 3) {
       //获取年龄
-      var myDate = new Date();
-      var month = myDate.getMonth() + 1;
-      var day = myDate.getDate();
-      var age = myDate.getFullYear() - card.substring(6, 10) - 1;
+      let myDate = new Date();
+      let month = myDate.getMonth() + 1;
+      let day = myDate.getDate();
+      let age = myDate.getFullYear() - card.substring(6, 10) - 1;
       if (card.substring(10, 12) < month || card.substring(10, 12) == month && card.substring(12, 14) <= day) {
         age++;
       }
       return age;
     }
-  },
+  }
   //语音回调方法 获取光标位置
-  getCursortPosition: function (uniqueid, content) {
+  getCursortPosition(uniqueid, content) {
     content = decodeURIComponent(content);
     //获取光标所在位置
-    var oTxt1 = document.getElementById(uniqueid);
-    var cursurPosition = -1;
+    let oTxt1 = document.getElementById(uniqueid);
+    let cursurPosition = -1;
     if (oTxt1.selectionStart) {//非IE浏览器
       if (oTxt1.selectionStart != undefined) {
         cursurPosition = oTxt1.selectionStart;
       }
     } else {//IE
       if (document.selection != undefined) {
-        var range = document.selection.createRange();
+        let range = document.selection.createRange();
         range.moveStart("character", -oTxt1.value.length);
         cursurPosition = range.text.length;
       }
     }
     //合并内容
-    var uniqueidremork = "";
-    var remorklength = document.querySelector("#" + uniqueid).val().length;
+    let uniqueidremork = "";
+    let remorklength = document.querySelector("#" + uniqueid).val().length;
     if (remorklength <= 0) {
       uniqueidremork = document.querySelector("#" + uniqueid).val() + content;
     } else {
@@ -324,30 +320,30 @@ ToolsObj.prototype = {
       uniqueidremork = uniqueidremork.substring(0, 500);
     }
     document.querySelector("#" + uniqueid).val(uniqueidremork);
-  },
+  }
   // 判断是否参数是否是NaN
-  isNaN: function (n) {
+  isNaN(n) {
     return n !== n
-  },
+  }
   //根据xpath获取指定元素
-  evalXpath: function (STR_XPATH) {
-    var xresult = document.evaluate(STR_XPATH, document, null, XPathResult.ANY_TYPE, null);
-    var xnodes = [];
-    var xres;
+  evalXpath(STR_XPATH) {
+    let xresult = document.evaluate(STR_XPATH, document, null, XPathResult.ANY_TYPE, null);
+    let xnodes = [];
+    let xres;
     while (xres = xresult.iterateNext()) {
       xnodes.push(xres);
     }
 
     return xnodes;
-  },
+  }
   // len为生成字符串的长度默认为8，count为生成随机字符串的数量默认为1
   // 返回一个包含随机字符串的数组
-  randomFn: function (len, count) {
+  randomFn(len, count) {
     len = len || 8
     count = count || 1
-    var randomStr
-    var randomStrArr
-    var arr = [
+    let randomStr
+    let randomStrArr
+    let arr = [
       ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
       [
         'a',
@@ -407,7 +403,7 @@ ToolsObj.prototype = {
     ]
     let averageLen = Math.ceil(len / arr.length)
     let randomArr = []
-    for (var n = 0; n < count; n++) {
+    for (let n = 0; n < count; n++) {
       randomStrArr = []
       arr = arr.sort(function () {
         return Math.random() > 0.5
@@ -427,7 +423,7 @@ ToolsObj.prototype = {
       randomArr.push(randomStr)
     }
     return randomArr
-  },
+  }
   //格式化日期 value为日期 link为链接符
   formatterDate(value, link = '-') {
     let date = new Date(value);
@@ -441,7 +437,7 @@ ToolsObj.prototype = {
       dateStr = year + link + ('0' + month).slice(-2) + link + ('0' + day).slice(-2);
     }
     return dateStr;
-  },
+  }
   getCurrentWeek() {
     //获取当前周的日期
     let weekDate = [];
@@ -453,11 +449,11 @@ ToolsObj.prototype = {
       }
     }
     console.log(weekDate);
-  },
+  }
   //深合并对象 
   deepMergeObjcet(target, source, optionsArgument) {
     function isMergeableObject(val) {
-      var nonNullObject = val && typeof val === 'object'
+      let nonNullObject = val && typeof val === 'object'
 
       return nonNullObject
         && Object.prototype.toString.call(val) !== '[object RegExp]'
@@ -469,12 +465,12 @@ ToolsObj.prototype = {
     }
 
     function cloneIfNecessary(value, optionsArgument) {
-      var clone = optionsArgument && optionsArgument.clone === true
+      let clone = optionsArgument && optionsArgument.clone === true
       return (clone && isMergeableObject(value)) ? deepmerge(emptyTarget(value), value, optionsArgument) : value
     }
 
     function defaultArrayMerge(target, source, optionsArgument) {
-      var destination = target.slice()
+      let destination = target.slice()
       source.forEach(function (e, i) {
         if (typeof destination[i] === 'undefined') {
           destination[i] = cloneIfNecessary(e, optionsArgument)
@@ -488,7 +484,7 @@ ToolsObj.prototype = {
     }
 
     function mergeObject(target, source, optionsArgument) {
-      var destination = {}
+      let destination = {}
       if (isMergeableObject(target)) {
         Object.keys(target).forEach(function (key) {
           destination[key] = cloneIfNecessary(target[key], optionsArgument)
@@ -505,9 +501,9 @@ ToolsObj.prototype = {
     }
 
     function deepmerge(target, source, optionsArgument) {
-      var array = Array.isArray(source);
-      var options = optionsArgument || { arrayMerge: defaultArrayMerge }
-      var arrayMerge = options.arrayMerge || defaultArrayMerge
+      let array = Array.isArray(source);
+      let options = optionsArgument || { arrayMerge: defaultArrayMerge }
+      let arrayMerge = options.arrayMerge || defaultArrayMerge
 
       if (array) {
         return Array.isArray(target) ? arrayMerge(target, source, optionsArgument) : cloneIfNecessary(source, optionsArgument)
@@ -528,4 +524,4 @@ ToolsObj.prototype = {
     return deepmerge(target, source, optionsArgument);
   }
 }
-var tools = new ToolsObj()
+let tools = new ToolsObj()
